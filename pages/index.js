994ad8b53b29fdecc89f745733632386a0e35e92
Daniel Natera <43421626+danielnatera@users.noreply.github.com>
2021-06-18 02:15:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,6 @@ const Home = () => {
       <Head>
         <title> Mercadillo</title>
       </Head>
-      <Body>
-
-        {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
-        <script src="/__/firebase/8.6.7/firebase-app.js"></script>
-
-        {/* <!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries --> */}
-
-        {/* <!-- Initialize Firebase --> */}
-        <script src="/__/firebase/init.js"></script>
-      </Body>
     </div>
   )
 }
